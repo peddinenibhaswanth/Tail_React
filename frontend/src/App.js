c4873ad44cec2d1480641751d2c3625e55943d58
Bhaswanth Peddinen <peddinenibhaswanth@gmail.com>
2025-11-02 13:30:00 +0530

    Set up main App component with router and layout structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,38 @@
-// Main App component
+import React, { useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import AppRoutes from "./routes";
+import Navbar from "./components/Common/Navbar";
+import Footer from "./components/Common/Footer";
+import Alert from "./components/Common/Alert";
+import { getCart } from "./redux/slices/cartSlice";
+import useAuth from "./hooks/useAuth";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/custom.css";
+
+function App() {
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    // Load cart if user is authenticated
+    if (isAuthenticated) {
+      dispatch(getCart());
+    }
+  }, [dispatch, isAuthenticated]);
+
+  return (
+    <Router>
+      <div className="App d-flex flex-column min-vh-100">
+        <Navbar />
+        <Alert />
+        <main className="flex-grow-1">
+          <AppRoutes />
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
